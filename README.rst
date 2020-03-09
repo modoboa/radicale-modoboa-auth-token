daeb36176f1588cfda22f6434398083843da39ea
Antoine Nguyen <tonio@ngyn.org>
2020-03-09 09:36:34 +0100

    Preparing future package.

--- README.rst
+++ README.rst
@@ -1,4 +1,26 @@
 radicale-modoboa-token-auth
 ===========================
 
-A token based authentication plugin for Radicale provided by Modoboa.
+A token based authentication plugin for Radicale provided by
+Modoboa. If the authentication fails, it will fallback to dovecot
+authentication plugin.
+
+Installation
+------------
+
+You can install this package from PyPi using the following command::
+
+   pip install radicale-modoboa-token-auth
+
+Configuration
+-------------
+
+Here is a configuration example::
+
+   [auth]
+   type = radicale_modoboa_token_auth
+
+   check_token_url = https://my.modoboa.com/api/v1/user-calendars/check_token/
+   token = MYTOKENHERE
+
+The `token` setting corresponds to a valid Modoboa API token.
--- setup.py
+++ setup.py
@@ -5,5 +5,22 @@ from distutils.core import setup
 
 setup(
     name="radicale_modoboa_token_auth",
-    packages=["radicale_modoboa_token_auth"]
+    version='0.1.0',
+    description='A token based authentication plugin for Radicale provided by Modoboa.',
+    url='https://github.com/modoboa/radicale-modoboa-auth-token',
+    author='Antoine Nguyen',
+    author_email='tonio@ngyn.org',
+    license='MIT',
+    packages=["radicale_modoboa_token_auth"],
+    install_requires=['radicale'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: POSIX',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
+    ]
 )
